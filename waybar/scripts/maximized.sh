64b30e39e1b2ba300bbc71f4f0c6710856a9eaa0
Darrel Griët <dgriet@gmail.com>
2025-03-22 16:08:37 +0100

    waybar: scripts: Disable maximized script on non-hyprland
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

--- waybar/scripts/maximized.sh
+++ waybar/scripts/maximized.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+hyprland_running=$(hyprctl activeworkspace -j)
+[ $? -eq 0 ]  || exit 1
+
 while true; do
     dunststatus=$(hyprctl activeworkspace -j | jq '.hasfullscreen')
     if [ $dunststatus = "false" ]; then
