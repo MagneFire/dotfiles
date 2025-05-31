5b90274af6e85385420611001a210ec1fec297f6
Darrel Griët <dgriet@gmail.com>
2025-05-31 15:52:20 +0200

    sway: scripts: Add insert workspace script
    Copied from
    https://github.com/ErikReider/Linux/blob/master/dotfiles/.config/sway/scripts/ws.py
    but patched to work correctly.
    For some reason inserting workspaces always resulted in exceptions never
    actually renaming workspaces.
    
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

---
+++ sway/scripts/ws.py
@@ -0,0 +1,54 @@
+from sys import argv
+from typing import List
+from i3ipc import Connection
+import traceback
+
+i3 = Connection()
+
+i3_workspaces = i3.get_workspaces()
+i3_workspaces.sort(key=lambda x: x.ipc_data["num"])
+workspaces: List[int] = [e.ipc_data["num"] for e in i3_workspaces]
+
+print(f"Workspaces {workspaces}")
+
+current_ws = 1
+for ws in i3_workspaces:
+    if ws.ipc_data["focused"]:
+        current_ws = ws.ipc_data["num"]
+
+
+print(f"Current workspace {current_ws}")
+
+def insert_ws(new_num: int):
+    # Check if workspace num already exists
+    try:
+        # Renames the next workspaces to n+1 when needed
+        def rename_ws(new_num: int, index: int):
+            if len(workspaces) > index + 1 and workspaces[index] + 1 == workspaces[index + 1]:
+                rename_ws(new_num, index + 1)
+
+            print(f"Renaming workspace {workspaces[index]} -> {workspaces[index] + 1}")
+            i3.command("rename workspace number {} to {}"
+                       .format(workspaces[index], workspaces[index] + 1))
+
+        rename_ws(new_num, workspaces.index(new_num))
+    except Exception as _:
+        print(f"Exception workspace {new_num}")
+        traceback.print_exc()
+
+        # Not found
+        pass
+
+    print(f"Creating workspace {new_num}")
+    # Create the new workspace
+    i3.command("workspace number {}".format(new_num))
+
+
+match argv[1]:
+    case "--insert-ws-next":
+        insert_ws(current_ws + 1)
+    case "--insert-ws-prev":
+        insert_ws(current_ws)
+    case _:
+        print("Unknown command")
+        exit(1)
